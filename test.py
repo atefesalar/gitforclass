e4850f3fb68951933b0d7e02d35b60962611e454
atefesalar <134092245+atefesalar@users.noreply.github.com>
2023-05-19 11:21:27 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 print("Add your name with a simple print like code below")
 print("Sassan Farahani!!!")
 
-#This is Test By seyedAli mousavi jazy 
-a = 'SeyedAli'
-b=  'mousavi'
-print(a,b)
+print("atefehsalar!")
